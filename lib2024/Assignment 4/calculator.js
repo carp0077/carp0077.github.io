0466a44f09afddf0f0480f97bc534898ebc4f916
carp0077 <carp0077@algonquinlive.com>
2023-04-22 08:25:28 -0400

    Added comments

--- lib2024/Assignment 4/calculator.js
+++ lib2024/Assignment 4/calculator.js
@@ -1,11 +1,15 @@
-function temperatureConverter(valNum) {
-  output = (valNum-32) / 1.8;
-}
-
-function validateForm(output, inputFahrenheit) {
-  if (inputFahrenheit.value.length == 0 || inputFahrenheit == "") {
-    document.getElementById("outputCelcius").innerHTML = "Please enter a number.";
+/*This function is called when the "Conver to Celcius" button is clicked.*/
+function validateForm(inputFahrenheit) {
+  /*Check that a valid number has been entered. If not, display an error message.*/
+  if (inputFahrenheit.value.length == 0 || inputFahrenheit == "") {    
+    alert("Please enter a number in degrees Fahrenheit.");
+  /*If a valid number has been entered, display result of temperatureConverter()*/
   } else {
     document.getElementById("outputCelcius").innerHTML = output;
   }
+}
+
+/*This function converts Fahrenheit to Celcius. It is called whenever anything is entered or changed in the input box, but it does not display the result until validateForm() is called.*/
+function temperatureConverter(valNum) {
+  output = (valNum-32) / 1.8;
 }
